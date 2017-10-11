31a23146f60c83ce46999706c16aaf58418c90d2
Fabiano Goes <fabianogoes@gmail.com>
2017-10-11 16:14:40 -0300

    criei o README como usar

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
-# ng-interceptor-security
+# Interceptor Security Angular 4
 
-Interceptor Security Angular 4
+### Como usar
+
+
+* Instalar   
+
+```shell
+npm install --save ng-interceptor-security
+```
+
+* Criar um Service    
+
+```shell
+ng g s redirect-interceptor
+```  
+
+
+* Implementar   
+
+```js
+...
+import { InterceptorSecurityService } from 'ng-interceptor-security';
+...
+
+export class RedirectInterceptorService extends InterceptorSecurityService  {
+
+  constructor(public router: Router) {
+    super(router);
+  }
+
+  getUrlLoginRedirect(): string {
+    return '/login';
+  }
+
+}
+```   
+
+* Adicionar a Service como provider no Module
+
+```js
+...
+import { RedirectInterceptorService } from './redirect-interceptor.service';
+...
+
+...
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RedirectInterceptorService,
+      multi: true
+    }
+  ],
+...
+
+```
+
+
+
+
+
+
+
+
--- src/interceptor-security.service.ts
+++ src/interceptor-security.service.ts
@@ -2,35 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/observable';
 import { Router } from '@angular/router';
-import {SimpleGlobal} from 'ng2-simple-global';
 import 'rxjs/add/operator/do';
 
 @Injectable()
-export class InterceptorSecurityService  implements HttpInterceptor {
+export abstract class InterceptorSecurityService implements HttpInterceptor {   
 
-      constructor(private router: Router) {
-        console.log('InterceptorSecurityService - constructor()...');
-      }
-    
-      intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
-        return next.handle(req).do((event: HttpEvent<any>) => {
-          if (event instanceof HttpResponse) {
-            console.log('InterceptorSecurityService - Request - OK');
+    constructor(public router: Router) {
+      console.log('InterceptorSecurityService - constructor()...');
+    }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      
+      return next.handle(req).do((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          console.log('InterceptorSecurityService - Request - OK');
+        }
+      }, (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+              console.log('InterceptorSecurityService - Request - 401');
+              this.router.navigate([ this.getUrlLoginRedirect() ]);
           }
-        }, (err: any) => {
-          if (err instanceof HttpErrorResponse) {
-            if (err.status === 401) {
-                console.log('InterceptorSecurityService - Request - 401');
-                this.router.navigate([ this.getUrlLoginRedirect() ]);
-            }
-          }
-        });
-    
-      }
-    
-      getUrlLoginRedirect(): string {
-        return '/login';
-      }
+        }
+      });
+
+    }
+
+    abstract getUrlLoginRedirect(): string;
 
 }
